9cba261c5d927ec1647ad331be013e5f30516548
dumi <ksnapd@gmail.com>
2021-11-04 19:15:53 +0200

    cleaning folders

---
+++ hangman/hangman_library.py
@@ -0,0 +1,77 @@
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_| \__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+                   
+                   
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']        
+
+countries = [
+    "Albania",	"Andorra",	"Armenia",	"Austria",	"Azerbaijan",	"Belarus",	"Belgium",
+    "BosniaAndHerzegovina",	"Bulgaria",	"Croatia",	"Cyprus",	"Czechia",	"Denmark", "Estonia", 
+    "Finland",	"France",	"Georgia",	"Germany",	"Greece",	"Hungary",	"Iceland",	"Ireland",	
+    "Italy",	"Kazakhstan",	"Kosovo",	"Latvia",	"Liechtenstein",	"Lithuania",	"Luxembourg",
+    "Malta",	"Moldova",	"Monaco",	"Montenegro",	"Netherlands",	"Macedonia",	 "Norway",	
+    "Poland",	"Portugal",	"Romania",	"Russia",	"SanMarino",	"Serbia",	"Slovakia",	"Slovenia",	
+    "Spain",	"Sweden",	"Switzerland",	"Turkey",	"Ukraine",	"UnitedKingdom", "VaticanCity"
+]
