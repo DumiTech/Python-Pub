a24450233e8f835b2ee9ffd50082ea0e3a1e263d
dumi <ksnapd@gmail.com>
2021-11-22 19:12:40 +0200

    quizz project

---
+++ The_Quiz_Project/main.py
@@ -0,0 +1,23 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for data in question_data:
+    question_text = data['text']
+    question_answer = data['answer']
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+    
+# print(question_bank[0].question_text)
+
+quiz_brain = QuizBrain(question_bank)
+    
+while quiz_brain.still_has_questions():
+    quiz_brain.next_question()
+    
+print("You completed the quiz")
+print(f"Your final score was: {quiz_brain.score}/{quiz_brain.question_number}")
+
+# print(question_bank)
---
+++ The_Quiz_Project/question_model.py
@@ -0,0 +1,7 @@
+class Question:
+    def __init__(self, question, answer):
+        self.question_text = question
+        self.question_answer = answer
+
+        # print(question_text)
+        # print(question_answer)
