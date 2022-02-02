129fb250e4440da2b8608b518141f8d70c7c759b
dumi <ksnapd@gmail.com>
2022-02-02 19:31:54 +0200

    Page objects

---
+++ Automation/Selenium_course_Fast_scalable_professional_grade_UI_automation_principles/pages/pom.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+
+from training_ground_page import TrainingGroundPage
+
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+# Test Setup
+browser = webdriver.Chrome(options=options)
+test_value = 'well done'
+
+# Test
+instruction_page = TrainingGroundPage(driver = browser)
+instruction_page.go()
+instruction_page.type_into_input(test_value)
+# instruction_page.click_button_1()
+text_from_input = instruction_page.get_input_text()
+assert text_from_input == test_value, f'Test Failed: Input did not match expected ({test_value}).'
+print('Test passed!')
