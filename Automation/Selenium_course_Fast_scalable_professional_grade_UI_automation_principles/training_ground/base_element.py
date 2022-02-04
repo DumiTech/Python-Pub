260a79ac01a6c31c6409684d46f5d82916dfc001
dumi <ksnapd@gmail.com>
2022-02-04 17:48:56 +0200

    training ground automation

---
+++ Automation/Selenium_course_Fast_scalable_professional_grade_UI_automation_principles/training_ground/base_element.py
@@ -0,0 +1,28 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC 
+
+class BaseElement(object):
+    def __init__(self, driver, value, by):
+        self.driver = driver
+        self.value = value
+        self.by = by
+        self.locator = (self.by, self.value)
+        
+        self.web_element = None
+        self.find()
+        
+    def find(self):
+        element = WebDriverWait(self.driver, 9).until(EC.visibility_of_element_located(locator=self.locator))
+        self.web_element = element
+        return None
+
+    def click(self):
+        element = WebDriverWait(self.driver, 9).until(EC.element_to_be_clickable(self.locator))
+        element.click()
+        return None
+    
+    @property
+    def text(self):
+        text = self.web_element.text
+        return text
---
+++ Automation/Selenium_course_Fast_scalable_professional_grade_UI_automation_principles/training_ground/pom_test.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+
+from training_ground_page_v2 import TrainingGroundPage
+
+import warnings
+
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+# Test Setup
+browser = webdriver.Chrome(options=options)
+test_value = 'well done'
+
+# Test
+instruction_page = TrainingGroundPage(browser)
+instruction_page.go()
+assert instruction_page.button1.text == 'Button1'
+instruction_page.button1.click()
+
+# browser.quit()
+
+
+
+##for training_ground_page.py import:
+# instruction_page.type_into_input(test_value)
+## instruction_page.click_button_1() 
+# text_from_input = instruction_page.get_input_text()
+# assert text_from_input == test_value, f'Test Failed: Input did not match expected ({test_value}).'
+# print('Test passed!')
