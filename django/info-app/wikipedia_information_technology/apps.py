048844e3c4b499ede18c5cab8a6693b523fc249a
dumiTech <cernei_dumitru@protonmail.com>
2022-08-01 20:09:43 +0300

    django info-app

---
+++ django/info-app/wikipedia_information_technology/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WikipediaInformationTechnologyConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'wikipedia_information_technology'
