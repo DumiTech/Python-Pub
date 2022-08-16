113a84f2a27300e15de051196d488ff0ca4cdf6f
dumiTech <cernei_dumitru@protonmail.com>
2022-08-16 09:28:04 +0300

    roman to integer

---
+++ leetcode/roman_2_integerV2.py
@@ -0,0 +1,29 @@
+class Roman2Integer:
+
+    def romanToInt(self, s):
+
+        dict = {
+            "M":1000,
+            "D":500,
+            "C":100,
+            "L":50,
+            "X":10,
+            "V":5,
+            "I":1
+        }
+
+        num, tmp = 0, "I"
+        # reversing => starting from the end towards the first taking each element
+        for c in s[::-1]:
+            num = num - dict[c] if dict[c] < dict[tmp] else num + dict[c]
+            print("num = {}, dict[c] = {}, dict[tmp] = {}".format(num, dict[c], dict[tmp]))
+            tmp = c
+
+        return num
+
+
+class Solution:
+ 
+    s = "IX"
+    obj = Roman2Integer()
+    print(obj.romanToInt(s))
